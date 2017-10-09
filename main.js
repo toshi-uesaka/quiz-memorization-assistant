dd7cb6834c6f76c4ea519200d358876104f27e34
kamitsutoshi <meronhei@yahoo.co.jp>
2017-10-09 22:08:50 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// phina.js をグローバル領域に展開
+phina.globalize();
+
+// MainScene クラスを定義
+phina.define('MainScene', {
+  superClass: 'CanvasScene',
+  init: function() {
+    this.superInit();
+    this.backgroundColor = '#444';
+    this.label = Label('Hello, phina.js!').addChildTo(this);
+    this.label.x = this.gridX.center();
+    this.label.y = this.gridY.center();
+    this.label.fill = 'white';
+  },
+});
+
+// メイン処理
+phina.main(function() {
+  // アプリケーション生成
+  var app = GameApp({
+    startLabel: 'main',
+  });
+  app.run();
+});
